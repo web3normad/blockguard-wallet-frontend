1f73e62a3c1ca11b60cdd9881d69a194b5ce624d
Emmanuel Doji <devopsnormad@gmail.com>
2024-09-12 14:00:11 +0100

    Changes to the router on App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { useState } from "react"; 
 import Home from "./pages/Home/Home";
